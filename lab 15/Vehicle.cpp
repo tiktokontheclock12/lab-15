f71522b338fc5cb978bf6dfdb1b8fa9bc1666d5c
steinke61502 <steinke61502@B124-22>
2025-04-10 10:43:00 -0400

    code for vechile cpp

--- lab 15/Vehicle.cpp
+++ lab 15/Vehicle.cpp
@@ -1,1 +1,30 @@
+// Vehicle.cpp
 #include "Vehicle.h"
+#include <iostream>
+using namespace std;
+
+Vehicle::Vehicle() : manufacturer("Unknown"), yearBuilt(0) {}
+
+Vehicle::Vehicle(string manu, int year) : manufacturer(manu), yearBuilt(year) {}
+
+void Vehicle::setManufacturer(string manu) {
+    manufacturer = manu;
+}
+
+void Vehicle::setYearBuilt(int year) {
+    yearBuilt = year;
+}
+
+string Vehicle::getManufacturer() const {
+    return manufacturer;
+}
+
+int Vehicle::getYearBuilt() const {
+    return yearBuilt;
+}
+
+void Vehicle::displayInfo() const {
+    cout << "Vehicle Information:" << endl;
+    cout << "Manufacturer: " << manufacturer << endl;
+    cout << "Year Built: " << yearBuilt << endl;
+}
