149fa0c27c04caa2c5ff5dbb371535b12b967975
steinke61502 <steinke61502@B124-22>
2025-04-10 10:41:22 -0400

    Coded Vehicle.h

--- lab 15/Vehicle.h
+++ lab 15/Vehicle.h
@@ -1,5 +1,27 @@
 #pragma once
-class Vehicle
-{
+// Vehicle.h
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+#include <string>
+using namespace std;
+
+class Vehicle {
+protected:
+    string manufacturer;
+    int yearBuilt;
+
+public:
+    Vehicle();
+    Vehicle(string manu, int year);
+
+    void setManufacturer(string manu);
+    void setYearBuilt(int year);
+
+    string getManufacturer() const;
+    int getYearBuilt() const;
+
+    virtual void displayInfo() const;
 };
 
+#endif
